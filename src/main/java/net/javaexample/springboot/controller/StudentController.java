48815e543eceddd2a498cd9d22a23ed06ae54c29
Jason Pis-an (NCS) <jason.pis-an@ph.ncs-i.com>
2023-10-31 23:25:34 +0800

    Springboot REST API return Java Bean

---
+++ src/main/java/net/javaexample/springboot/controller/StudentController.java
@@ -0,0 +1,19 @@
+package net.javaexample.springboot.controller;
+
+import net.javaexample.springboot.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("student")
+    public Student getStudent() {
+        Student student = new Student(
+                1,
+                "Jace",
+                "Pis-an"
+        );
+        return student;
+    }
+}
